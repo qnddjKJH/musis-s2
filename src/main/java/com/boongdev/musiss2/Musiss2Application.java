4aa1ac993e0158f2bb72103ae036c6feab9dc86c
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-03-22 20:31:24 +0900

    Init project

---
+++ src/main/java/com/boongdev/musiss2/Musiss2Application.java
@@ -0,0 +1,13 @@
+package com.boongdev.musiss2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Musiss2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Musiss2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/boongdev/musiss2/Musiss2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boongdev.musiss2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Musiss2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
